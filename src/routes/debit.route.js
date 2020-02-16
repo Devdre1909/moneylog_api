4631a1f668597ac46cb0fd4a7b6a73849c13132f
Oluwadamilare <adegoketemitope1909@gmail.com>
2020-02-16 19:45:07 +0100

    Include new route

--- src/routes/debit.route.js
+++ src/routes/debit.route.js
@@ -1,30 +1,76 @@
-const express = require('express');
-const debitSchema = require('../models/debit.model');
-const httpStatusCode = require('http-status-codes');
+const express = require("express");
+const httpStatusCode = require("http-status-codes");
+
+const debitSchema = require("../models/debit.model");
+const {
+    getByDate
+} = require("../middleware/parseMethod.middleware");
 
 const router = express.Router();
 
-router.post('/api/send/debit', (req, res) => {
-    if (!req.body) return res.status(httpStatusCode.BAD_REQUEST).json({
-        'status': httpStatusCode.getStatusText(httpStatusCode.BAD_REQUEST)
-    });
+router.post("/api/send/debit", (req, res) => {
+    if (!req.body)
+        return res.status(httpStatusCode.BAD_REQUEST).json({
+            status: httpStatusCode.getStatusText(httpStatusCode.BAD_REQUEST)
+        });
     let model = new debitSchema(req.body);
-    model.save()
+    model
+        .save()
         .then(doc => {
-            if (!doc || doc.length === 0) return res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
-                'status': httpStatusCode.getStatusText(httpStatusCode.INTERNAL_SERVER_ERROR)
-            });
+            if (!doc || doc.length === 0)
+                return res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
+                    status: httpStatusCode.getStatusText(
+                        httpStatusCode.INTERNAL_SERVER_ERROR
+                    )
+                });
             res.status(httpStatusCode.CREATED).json({
-                'status': 'created',
+                status: "created",
                 doc
-            })
+            });
         })
         .catch(err => {
             res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
-                'status': httpStatusCode.getStatusText(httpStatusCode.INTERNAL_SERVER_ERROR),
-                'error': err
-            })
+                status: httpStatusCode.getStatusText(
+                    httpStatusCode.INTERNAL_SERVER_ERROR
+                ),
+                error: err
+            });
+        });
+});
+
+router.get("/api/get/debit/all", (req, res) => {
+    debitSchema
+        .find()
+        .then(doc => {
+            res.status(httpStatusCode.OK).json({
+                result: doc
+            });
         })
+        .catch(err => {
+            res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
+                error: err
+            });
+        });
+});
+
+router.get("/api/get/debit/:date", (req, res) => {
+
+    if (!req.params)
+        return res.status(httpStatusCode.BAD_REQUEST).json({
+            err: httpStatusCode.getStatusText(httpStatusCode.BAD_REQUEST),
+        });
+    debitSchema
+        .find()
+        .then(docs => {
+            res.status(httpStatusCode.OK).json({
+                result: getByDate(docs, req.params.date)
+            });
+        })
+        .catch(err => {
+            res.status(httpStatusCode.INTERNAL_SERVER_ERROR).json({
+                error: err.stack
+            });
+        });
 });
 
 module.exports = router;
